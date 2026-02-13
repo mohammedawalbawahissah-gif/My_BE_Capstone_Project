8e816de38bf0609881b123a13a3fe6912aa3bf62
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-13 23:46:07 +0000

    git commit -m "serializers, model changes and urls"

--- neomat_care/core/views.py
+++ neomat_care/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from rest_framework import serializers
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class RegisterUserAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+    permission_classes = [AllowAny] 
