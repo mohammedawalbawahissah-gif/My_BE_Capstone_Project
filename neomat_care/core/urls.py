840895db3c9a165d52fe201fde637837b0ed078f
Mohammed Awal <mohammedawalbawahissah@gmail.com>
2026-02-19 20:04:06 +0000

    git commit -m "Add referral suggestion endpoint to URLs"

--- neomat_care/core/urls.py
+++ neomat_care/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import RegisterUserAPIView
+from .views import SuggestReferralView
 
 urlpatterns = [
     path('register/', RegisterUserAPIView.as_view(), name='register'),
     path('login/', obtain_auth_token, name='api_token_auth'),
+    path("referrals/suggest/", SuggestReferralView.as_view(), name="suggest-referral"),
 ]
